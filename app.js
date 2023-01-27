61e5fb8568bf208d9af035d32836faea8addfdb8
Joe Calvi <joecalvi@me.com>
2023-01-27 11:57:44 -0700

    add total collected to layout

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
+let upgradesShop = [
+    {
+        name: "",
+        cost: 100,
+        collectionRate: 3,
+        type: "click"
+    },
+    {
+        name: "",
+        cost: 100,
+        collectionRate: 3,
+        type: "click"
+    },
+    {
+        name: "",
+        cost: 100,
+        collectionRate: 3,
+        type: "auto"
+    },
+    {
+        name: "",
+        cost: 100,
+        collectionRate: 3,
+        type: "auto"
+    }
+]
 
+let purchaseUpgrades = []
+
+let collectedResource = 0
+let totalCollectedResource = 0
 
 
 
